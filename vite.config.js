7463e10c235f226ba31f77d1135861d623da5202
Antonio Abbruzzese <aabbruzzeselive@gmail.com>
2025-05-09 10:02:14 +0200

    Configurazione per gh-pages: aggiunto deployment automatico

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    // Genera source maps per una migliore gestione degli errori
-    sourcemap: true,
-    // Aggiungi questo per rendere i percorsi relativi funzionanti in GitHub Pages
-    assetsDir: 'assets'
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
+  base: '/MindTop.github.io/',
 })
